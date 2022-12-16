a594b98aa3d37fb2006cb64f67896f57076dfd74
Abdulazeez <abdulazeezoshinowo2@gmail.com>
2022-12-16 04:55:25 +0100

    create models

---
+++ lib/models/custom_error.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+class CustomError extends Equatable {
+  final String errMsg;
+
+  const CustomError({
+    this.errMsg = '',
+  });
+
+  @override
+  List<Object> get props => [errMsg];
+
+  @override
+  bool get stringify => true;
+}
---
+++ lib/models/direct_geocoding.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+class DirectGeocoding extends Equatable {
+  final String name;
+  final String lat;
+  final String lon;
+  final String country;
+
+  const DirectGeocoding({
+    required this.name,
+    required this.lat,
+    required this.lon,
+    required this.country,
+  });
+
+  factory DirectGeocoding.fromJson(List<dynamic> json) {
+    final Map<String, dynamic> data = json[0];
+
+    return DirectGeocoding(
+        name: data['name'],
+        lat: data['lat'],
+        lon: data['lon'],
+        country: data['country']);
+  }
+
+  @override
+  List<Object> get props => [name, lat, lon, country];
+
+  @override
+  bool get stringify => true;
+}
